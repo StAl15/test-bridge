75881ba20c480c1efccf0dd32e00a09f21dfb553
StAl15 <stihinaleksander@yandex.ru>
2023-08-13 11:32:08 +0500

    add neuromodel

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+from collections import deque
+from webcam_demo_1 import parse_args
+from threading import Thread
+from webcam_demo import show_results
 import numpy as np
+from webcam_demo import init_model
 import cv2
+from webcam_demo import inference
 from fastapi import FastAPI, WebSocket
 
+# inference()
 app = FastAPI()
+args = {
+    "config_path": "config.json",
+    "device": "your_device",
+    "camera_id": 0,
+    "sample_length": 10,
+    "drawing_fps": 30,
+    "inference_fps": 15,
+    "openvino": False
+}
+model = init_model(args['config_path'])
+frame_queue = deque()
 
 
 class Connection:
@@ -16,13 +34,20 @@ class Connection:
 
             # Декодируем изображение из сжатого формата (например, JPEG)
             image_np = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), cv2.IMREAD_COLOR)
-
+            print("inference")
+            frame_queue.append(np.array(cv2.resize(image_np, (224, 224))[:, :, ::-1]))
+            rez = inference(model)
+            # pw = Thread(target=show_results, args=(image_np,), daemon=True)
+            # pr = Thread(target=inference, args=(model,), daemon=True)
+            # pr.start()
+            print(rez)
             if image_np is not None:
-                self.strings = f"Received image of size {image_np.shape[1]}x{image_np.shape[0]}"
+                self.strings = f"{rez}"
             else:
                 self.strings = "Failed to decode image"
 
             await self.send_strings()
+        return
 
     async def send_strings(self):
         await self.websocket.send_json(self.strings)
